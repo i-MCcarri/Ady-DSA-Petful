6416f0236c4c6fd9694801cd229b5adb37a92c8c
Ady <ady@gms-macbook-pro.lan>
2020-12-04 02:17:46 -0800

    fixed typo

--- petful-server-template/modules/queue/Queue.js
+++ petful-server-template/modules/queue/Queue.js
@@ -2,74 +2,62 @@ class _Node {
   constructor(value) {
       this.value = value;
       this.next = null;
+      this.prev = null;
   }
 }
 
 class Queue {
   constructor() {
     // Set initial data.
-    this.front = null;
-    this.end = null;
+    this.first = null;
+    this.last = null;
   }
 
   enqueue(data) {
       // Add some data to the queue.
       const node = new _Node(data);
 
-      if (this.front === null) {
-          this.front = node;
+      if (this.first === null) {
+          this.first = node;
       }
 
-      if (this.end) {
-          this.end.next = node;
+      if (this.last) {
+          this.last.next = node;
       }
-      //make the new node the end item on the queue
-      this.end = node;
+      //make the new node the last item on the queue
+      this.last = node;
   }
 
   dequeue() {
       //if the queue is empty, there is nothing to return
-     if (this.front === null) {
+     if (this.first === null) {
          return;
      }
-     const node = this.front;
-     this.front = this.front.next;
-      //if this is the end item in the queue
-     if (node === this.end) {
-         this.end = null;
+     const node = this.first;
+     this.first = this.first.next;
+      //if this is the last item in the queue
+     if (node === this.last) {
+         this.last = null;
      }
      return node.value;
   }
 
   show() {
     // Return the next item in the queue.
-    return this.front.value;
+    return this.first.value;
   }
 
   all() {
+    let all = [];
     let node = this.first;
-    const arr = [];
-     while (node) {
-         arr.push(node.value);
-         node = node.next;
-     }
-   return arr;
+    all.push(node.value);
+    node = node.next;
+    while (node) {
+      all.push(node.value);
+      node = node.next;
+    }
+   return all;
   }
 }
-    
-
-// let test = new Queue(); 
-//console.log('initial size: ', test.size());
-//console.log('initially empty: ', test.isEmpty());
-// test.enqueue(1);
-// test.enqueue(2);
-// test.enqueue(3);
-// console.log(test.end)
-//console.log('size: ', test.size());
-//console.log('next element to be removed is: ', test.peek());
-//console.log(test.all());
-//console.log(test.size());
-
 
 module.exports = Queue
- 
