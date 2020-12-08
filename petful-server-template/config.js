5bf669f179a7c9ff06e843974c0642fdba56d1e8
Ady <ady@gms-macbook-pro.lan>
2020-12-08 01:26:58 -0800

    deploy

--- petful-server-template/config.js
+++ petful-server-template/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+    CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://rocky-peak-50014.herokuapp.com'
   } 
